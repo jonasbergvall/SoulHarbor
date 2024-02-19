1cf760bfd1f4283da65190998e88ba773a78a30e
jonasbergvall <jonas@bergvalls.com>
2024-02-19 10:04:04 +0100

    Update WhistlePeep.py

--- WhistlePeep.py
+++ WhistlePeep.py
@@ -2,41 +2,42 @@ import streamlit as st
 from datetime import datetime
 import pandas as pd
 
-# Funktion för att spara data till fil
-def save_data(user_mood, user_data):
-    # Läs befintlig data från filen om det finns
+# Function to save data to a file
+def save_data(user_name, user_mood, user_data):
+    # Read existing data from the file if it exists
     try:
         existing_data = pd.read_csv('user_data.csv')
     except FileNotFoundError:
-        existing_data = pd.DataFrame(columns=['Date', 'Mood', 'User Data'])
+        existing_data = pd.DataFrame(columns=['Date', 'User Name', 'Mood', 'User Data'])
 
-    # Lägg till ny data endast om användaren klickade på Analysera
-    if st.button('Analysera'):
-        # Automatisk identifiering av dagens datum och tid
+    # Add new data only if the user clicked on Analyze
+    if st.button('Analyze'):
+        # Automatic identification of the current date and time
         current_date_time = datetime.now()
         formatted_date_time = current_date_time.strftime("%Y-%m-%d %H:%M:%S")
 
-        # Lägg till ny data
-        new_entry = pd.DataFrame([[formatted_date_time, user_mood, user_data]],
-                                 columns=['Date', 'Mood', 'User Data'])
+        # Add new data
+        new_entry = pd.DataFrame([[formatted_date_time, user_name, user_mood, user_data]],
+                                 columns=['Date', 'User Name', 'Mood', 'User Data'])
         updated_data = pd.concat([existing_data, new_entry], ignore_index=True)
 
-        # Spara till fil
+        # Save to file
         updated_data.to_csv('user_data.csv', index=False)
 
-        st.write('Analysresultat sparad!')
+        st.write('Analysis result saved!')
 
-# Användare anger sin känsla
-user_mood = st.slider('Hur mår du idag?', 0, 100, 50)
+# User inputs
+user_name = st.text_input('Enter your name:', 'Anonymous')
+user_mood = st.slider('How are you feeling today?', 0, 100, 50)
 
-# Skapa gränssnittet
+# Create the interface
 st.title('WhistlePeep - An Early Warning System for Work Environment')
 
-# Användarens datainmatning
-user_data = st.text_area('Skriv något om dagen:', '')
+# User's data input
+user_data = st.text_area('Write something about your day:', '')
 
-# Visa sammanställd data
-st.subheader('Sammanställd data:')
-save_data(user_mood, user_data)
+# Display compiled data
+st.subheader('Compiled data:')
+save_data(user_name, user_mood, user_data)
 compiled_data = pd.read_csv('user_data.csv')
 st.write(compiled_data)
