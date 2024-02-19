ae8935f9e44ff2cf6c1d4c243a62e07a61a7a8a2
jonasbergvall <jonas@bergvalls.com>
2024-02-19 15:12:09 +0100

    Update WhistlePeep.py

--- WhistlePeep.py
+++ WhistlePeep.py
@@ -2,21 +2,23 @@ import streamlit as st
 import pandas as pd
 import datetime
 
-# Global list to store user data
-user_data = []
-
 # Function to get user location (dummy data for illustration purposes)
 def get_user_location():
     return "Dummy Location"
 
 # Function to save user data
-def save_user_data(new_entry):
-    global user_data
-    user_data.append(new_entry)
+@st.cache(allow_output_mutation=True)
+def save_user_data(new_entry, user_data):
+    user_data = user_data.append(new_entry, ignore_index=True)
+    return user_data
 
 # Main Streamlit app
 def main():
     st.title("WhistlePeep - Employee Well-being Tracker")
+
+    # Get or create user data DataFrame
+    if 'user_data' not in st.session_state:
+        st.session_state.user_data = pd.DataFrame(columns=['Date', 'Mood', 'Location'])
 
     # Get user name
     user_name = st.text_input("Enter your name:")
@@ -31,14 +33,14 @@ def main():
     new_entry = {'Date': datetime.datetime.now(), 'Mood': user_mood, 'Location': user_location}
 
     # Save user data
-    save_user_data(new_entry)
+    st.session_state.user_data = save_user_data(new_entry, st.session_state.user_data)
 
     # Display user data
     st.write("User Data:")
-    st.write(pd.DataFrame(user_data))
+    st.write(st.session_state.user_data)
 
     # Display mood chart
-    st.line_chart(pd.DataFrame(user_data)['Mood'], use_container_width=True).set_ylim(0, 100)
+    st.line_chart(st.session_state.user_data['Mood'], use_container_width=True).set_ylim(0, 100)
 
 # Run the app
 if __name__ == "__main__":
