b0bd91d4548025c3fe54df2a36ad8f049b7f274d
jonasbergvall <jonas@bergvalls.com>
2024-02-19 14:56:24 +0100

    Update WhistlePeep.py

--- WhistlePeep.py
+++ WhistlePeep.py
@@ -8,36 +8,37 @@ def get_user_location():
     return {"latitude": 0, "longitude": 0}
 
 # Function to save user data
-def save_user_data():
-    new_entry = {
-        "User": st.session_state.user_name,
-        "Date": st.session_state.date,
-        "Mood": st.session_state.user_mood,
-        "Location": get_user_location(),
-    }
-
-    # Create DataFrame if not exists
-    if 'user_data' not in st.session_state:
-        st.session_state.user_data = pd.DataFrame(columns=["User", "Date", "Mood", "Location"])
-
-    # Append new entry to user_data
-    st.session_state.user_data = st.session_state.user_data.append(new_entry, ignore_index=True)
+@st.cache(allow_output_mutation=True)
+def save_user_data(user_data, new_entry):
+    user_data.append(new_entry)
 
 # Set user name and date
-st.session_state.user_name = st.text_input("User Name:")
-st.session_state.date = st.date_input("Date:")
+user_name = st.text_input("User Name:")
+date = st.date_input("Date:")
 
 # Set user mood using a slider
-st.session_state.user_mood = st.slider("How was your day?", 0, 100, 50)
+user_mood = st.slider("How was your day?", 0, 100, 50)
 
 # Save user data on button click
 if st.button("Analyze"):
-    save_user_data()
+    new_entry = {
+        "User": user_name,
+        "Date": date,
+        "Mood": user_mood,
+        "Location": get_user_location(),
+    }
+
+    # Load existing user data
+    user_data = st.session_state.user_data if 'user_data' in st.session_state else []
+
+    # Append new entry to user_data
+    save_user_data(user_data, new_entry)
+    st.session_state.user_data = user_data
 
 # Display user data chart
-if 'user_data' in st.session_state and not st.session_state.user_data.empty:
+if 'user_data' in st.session_state and st.session_state.user_data:
     st.write("User Data Chart:")
-    
+
     # Create a Plotly figure
     fig = px.line(st.session_state.user_data, x='Date', y='Mood', color='User', labels={'Mood': 'User Mood'})
 
