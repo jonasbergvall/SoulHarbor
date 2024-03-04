14ecaef70dba23ff98c2dc57dec3117f2c677c5e
jonasbergvall <jonas@bergvalls.com>
2024-03-04 18:45:41 +0100

    Update SoulHarbor.py

--- SoulHarbor.py
+++ SoulHarbor.py
@@ -1,10 +1,9 @@
 import streamlit as st
 import pandas as pd
+import graphviz as gv
 
 # Hämta data från CSV-fil
-
 samarbete_data = pd.read_csv("samarbetsdata.csv", index_col="Deltagare")
-
 
 # Ange deltagare och ID för visualisering
 deltagare = st.sidebar.selectbox("Välj deltagare", samarbete_data.index)
@@ -13,28 +12,45 @@ id = samarbete_data.loc[deltagare, "ID"]
 # Visa information om vald deltagare
 st.header(f"Samarbetsdata för {deltagare} (ID: {id})")
 
-# Skapa matris för visuell representation av samarbeten
-samarbete_matris = samarbete_data.iloc[:, 2:].values
+# Skapa Graphviz-kod för nätverksgraf
+dot = """
+digraph G {
+  node [shape=circle, fontsize=12];
+"""
 
-# Visa matrisen
-st.table(samarbete_matris)
+# Lägg till noder till Graphviz-kod
+for deltagare in samarbete_data.index:
+    dot += f"  {deltagare};\n"
 
-# Loopa igenom deltagare och samarbeten
-for i in range(samarbete_matris.shape[0]):
-    for j in range(samarbete_matris.shape[1]):
-        # Visa detaljerad information om samarbete
-        if samarbete_matris[i, j] == 1:
-            st.markdown(f"- Samarbete med {samarbete_data.index[j]}")
+# Lägg till kanter till Graphviz-kod
+for i in range(samarbete_data.shape[0]):
+    for j in range(samarbete_data.shape[1]):
+        if samarbete_data.iloc[i, j] == 1:
+            dot += f"  {samarbete_data.index[i]} -> {samarbete_data.index[j]};\n"
 
-# Visa information om eventuella nya samarbeten
+dot += "}"
+
+# Generera PNG-bild från Graphviz-kod
+png = gv.render(dot, format='png').pipe(lambda x: x.decode('utf-8'))
+
+# Visa information om samarbeten
+st.markdown(f"- Samarbeten med:")
+for i in range(samarbete_data.shape[0]):
+    for j in range(samarbete_data.shape[1]):
+        if samarbete_data.iloc[i, j] == 1:
+            st.markdown(f"    - {samarbete_data.index[j]}")
+
+# Visa nätverksgraf
+st.image(png)
+
+# Visa detaljerad information om samarbeten
 nya_samarbeten = []
-for i in range(samarbete_matris.shape[0]):
-    for j in range(samarbete_matris.shape[1]):
-        if samarbete_matris[i, j] == 1 and samarbete_data.loc[samarbete_data.index[i], f"Samarbete{j+1}"] == 0:
+for i in range(samarbete_data.shape[0]):
+    for j in range(samarbete_data.shape[1]):
+        if samarbete_data.iloc[i, j] == 1 and samarbete_data.loc[samarbete_data.index[i], f"Samarbete{j+1}"] == 0:
             nya_samarbeten.append((samarbete_data.index[i], samarbete_data.index[j]))
 
 if nya_samarbeten:
     st.header("Nya samarbeten")
     for samarbeten in nya_samarbeten:
         st.markdown(f"- {samarbeten[0]} och {samarbeten[1]}")
-
