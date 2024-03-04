1e06cb826018fd96e66079a67152e4ea3041c7a5
jonasbergvall <jonas@bergvalls.com>
2024-03-04 18:56:54 +0100

    Update SoulHarbor.py

--- SoulHarbor.py
+++ SoulHarbor.py
@@ -3,7 +3,7 @@ import pandas as pd
 import graphviz as gv
 
 # Hämta data från CSV-fil
-samarbete_data = pd.read_csv("samarbetsdata.csv", index_col="Deltagare")
+samarbete_data = pd.read_csv("samarbetsdata.csv")
 
 # Ange deltagare och ID för visualisering
 deltagare = st.sidebar.selectbox("Välj deltagare", samarbete_data.index)
@@ -19,14 +19,14 @@ digraph G {
 """
 
 # Lägg till noder till Graphviz-kod
-for deltagare in samarbete_data.index:
+for deltagare in samarbete_data.columns:
     dot += f"  {deltagare};\n"
 
 # Lägg till kanter till Graphviz-kod
 for i in range(samarbete_data.shape[0]):
     for j in range(samarbete_data.shape[1]):
         if samarbete_data.iloc[i, j] == 1:
-            dot += f"  {samarbete_data.index[i]} -> {samarbete_data.index[j]};\n"
+            dot += f"  {samarbete_data.columns[i]} -> {samarbete_data.columns[j]};\n"
 
 dot += "}"
 
@@ -38,7 +38,7 @@ st.markdown(f"- Samarbeten med:")
 for i in range(samarbete_data.shape[0]):
     for j in range(samarbete_data.shape[1]):
         if samarbete_data.iloc[i, j] == 1:
-            st.markdown(f"    - {samarbete_data.index[j]}")
+            st.markdown(f"    - {samarbete_data.columns[j]}")
 
 # Visa nätverksgraf
 st.image(png)
@@ -47,8 +47,8 @@ st.image(png)
 nya_samarbeten = []
 for i in range(samarbete_data.shape[0]):
     for j in range(samarbete_data.shape[1]):
-        if samarbete_data.iloc[i, j] == 1 and samarbete_data.loc[samarbete_data.index[i], f"Samarbete{j+1}"] == 0:
-            nya_samarbeten.append((samarbete_data.index[i], samarbete_data.index[j]))
+        if samarbete_data.iloc[i, j] == 1 and samarbete_data.loc[samarbete_data.columns[i], f"Samarbete{j+1}"] == 0:
+            nya_samarbeten.append((samarbete_data.columns[i], samarbete_data.columns[j]))
 
 if nya_samarbeten:
     st.header("Nya samarbeten")
